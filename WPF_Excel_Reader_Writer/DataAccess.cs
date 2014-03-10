b5bcbcf2b5a638ed53c49442226faad83cfcad90
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-03-10 14:36:11 +0530

    Code added

---
+++ WPF_Excel_Reader_Writer/DataAccess.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using System.Data.OleDb;
+using System.Collections.ObjectModel;
+
+namespace WPF_Excel_Reader_Writer
+{
+
+    public class Employee
+    {
+        public int EmpNo { get;set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+        public string DeptName { get; set; }
+    }
+    public class DataAccess
+    {
+        OleDbConnection Conn;
+        OleDbCommand Cmd;
+
+        public DataAccess()
+        {
+            Conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=F:\\FromC\\VS2013\\WPF_45_DEMOS\\Employee.xlsx;Extended Properties=\"Excel 12.0 Xml;HDR=YES;\""); 
+        }
+
+        /// <summary>
+        /// Method to Get All the Records from Excel
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ObservableCollection<Employee>> GetDataFormExcelAsync()
+        {
+            ObservableCollection<Employee> Employees = new ObservableCollection<Employee>(); 
+            await Conn.OpenAsync();
+            Cmd = new OleDbCommand();
+            Cmd.Connection = Conn;
+            Cmd.CommandText = "Select * from [Sheet1$]";
+            var Reader = await Cmd.ExecuteReaderAsync();
+            while (Reader.Read())
+            {
+                Employees.Add(new Employee() { 
+                    EmpNo = Convert.ToInt32(Reader["EmpNo"]),
+                    EmpName = Reader["EmpName"].ToString(),
+                    DeptName = Reader["DeptName"].ToString(),
+                    Salary = Convert.ToInt32(Reader["Salary"]) 
+                });
+            }
+            Reader.Close();
+            Conn.Close();
+            return Employees;
+        }
+
+        /// <summary>
+        /// Method to Insert Record in the Excel
+        /// S1. If the EmpNo =0, then the Operation is Skipped.
+        /// S2. If the Employee is already exist, then it is taken for Update
+        /// </summary>
+        /// <param name="Emp"></param>
+        public async Task<bool> InsertOrUpdateRowInExcelAsync(Employee Emp)
+        {
+            bool IsSave = false;
+            //S1
+            if (Emp.EmpNo != 0)
+            {
+                await Conn.OpenAsync();
+                Cmd = new OleDbCommand();
+                Cmd.Connection = Conn;
+                Cmd.Parameters.AddWithValue("@EmpNo", Emp.EmpNo);
+                Cmd.Parameters.AddWithValue("@EmpName", Emp.EmpName);
+                Cmd.Parameters.AddWithValue("@Salary", Emp.Salary);
+                Cmd.Parameters.AddWithValue("@DeptName", Emp.DeptName);
+                //S2
+                if (!CheckIfRecordExistAsync(Emp).Result)
+                {
+                    Cmd.CommandText = "Insert into [Sheet1$] values (@EmpNo,@EmpName,@Salary,@DeptName)";
+                }
+                else
+                {
+                    if (Emp.EmpName != String.Empty || Emp.DeptName != String.Empty)
+                    {
+                        Cmd.CommandText = "Update [Sheet1$] set EmpNo=@EmpNo,EmpName=@EmpName,Salary=@Salary,DeptName=@DeptName where EmpNo=@EmpNo";
+                    }
+                }
+                int result = await Cmd.ExecuteNonQueryAsync();
+                if (result > 0)
+                {
+                    IsSave = true;
+                }
+                Conn.Close();
+            }
+            return IsSave;
+
+        }
+
+         
+
+        /// <summary>
+        /// The method to check if the record is already available 
+        /// in the workgroup
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <returns></returns>
+        private async Task<bool> CheckIfRecordExistAsync(Employee emp)
+        {
+            bool IsRecordExist = false;
+            Cmd.CommandText = "Select * from [Sheet1$] where EmpNo=@EmpNo";
+            var Reader = await Cmd.ExecuteReaderAsync();
+            if (Reader.HasRows)
+            {
+                IsRecordExist = true;
+            }
+             
+            Reader.Close();
+            return IsRecordExist;
+        }
+    }
+     
+}
---
+++ WPF_Excel_Reader_Writer/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Excel_Reader_Writer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //The object of the DataAccess class
+        DataAccess objDs;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //The Employee Object for Edit
+        Employee emp = new Employee();
+        /// <summary>
+        /// On Load get data from the Excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            objDs = new DataAccess(); 
+            try
+            {
+                    dgEmp.ItemsSource = objDs.GetDataFormExcelAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// TO Synchronize the Excel Workbook with the Application 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private   void btnsync_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               dgEmp.ItemsSource =   objDs.GetDataFormExcelAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Read Data entered in each Cell
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgEmp_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                    FrameworkElement eleEno = dgEmp.Columns[0].GetCellContent(e.Row);
+                    if (eleEno.GetType() == typeof(TextBox))
+                    {
+                        emp.EmpNo = Convert.ToInt32(((TextBox)eleEno).Text);
+                    }
+
+                    FrameworkElement eleEname = dgEmp.Columns[1].GetCellContent(e.Row);
+                    if (eleEname.GetType() == typeof(TextBox))
+                    {
+                        emp.EmpName = ((TextBox)eleEname).Text;
+                    }
+
+                    FrameworkElement eleSal = dgEmp.Columns[2].GetCellContent(e.Row);
+                    if (eleSal.GetType() == typeof(TextBox))
+                    {
+                        emp.Salary = Convert.ToInt32(((TextBox)eleSal).Text);
+                    }
+
+                    FrameworkElement eleDname = dgEmp.Columns[3].GetCellContent(e.Row);
+                    if (eleDname.GetType() == typeof(TextBox))
+                    {
+                        emp.DeptName = ((TextBox)eleDname).Text;
+                    }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Get the Complete row
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgEmp_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            try
+            {
+              bool IsSave = objDs.InsertOrUpdateRowInExcelAsync(emp).Result;
+              if (IsSave)
+              {
+                  MessageBox.Show("Record Saved Successfully");
+              }
+              else
+              {
+                  MessageBox.Show("Problem Occured");
+              }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        /// <summary>
+        /// Select the Recod for the Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgEmp_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            emp = dgEmp.SelectedItem as Employee;
+        }
+    }
+}
